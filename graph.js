e78286c57badb9eeccc31faa21fdcf338132f4d9
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-03-16 14:26:56 +0800

    modified:   graph.js

--- graph.js
+++ graph.js
@@ -8,7 +8,10 @@ const Point = class {
 		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
 	}
 	static NewRandom() {
-		return new Point(Math.random(), Math.random());
+		return new Point(
+			Math.random(),
+			Math.random(),
+		);
 	}
 };
 
